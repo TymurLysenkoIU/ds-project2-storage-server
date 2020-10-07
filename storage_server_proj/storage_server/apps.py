02588cb9d1a0450202f3c5a60fd871a7dff0393d
Tymur Lysenko <tymur.lysenko@gmail.com>
2020-10-07 18:06:37 +0300

    Send request, when app is started

--- storage_server_proj/storage_server/apps.py
+++ storage_server_proj/storage_server/apps.py
@@ -1,5 +1,24 @@
 from django.apps import AppConfig
 
+import sys
+
+import requests as r
+
+from .config import NAME_SERVER_ADDRESS
+
 
 class StorageServerConfig(AppConfig):
-    name = 'storage_server'
+  name = 'storage_server_proj.storage_server'
+
+  def ready(self):
+    try:
+      # TODO: when the endpoint will be implemented get back to this place
+      # r.get(
+      #   url=f"{NAME_SERVER_ADDRESS}/connect",
+      #   params={},
+      # )
+      pass
+    except:
+      print(f"Unable to connect to the name server: {NAME_SERVER_ADDRESS}")
+      print("Exiting...")
+      sys.exit(1)
--- storage_server_proj/storage_server/config.py
+++ storage_server_proj/storage_server/config.py
@@ -1,3 +1,5 @@
 from os import environ
 
 STORAGE_BASE_PATH = environ.get("STORAGE_BASE_PATH", "/home/vsftpd/")
+
+NAME_SERVER_ADDRESS = "http://name-server"
